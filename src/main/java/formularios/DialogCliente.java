51bc700e0e33799510f32bb90a05cb1c6f4e4fbd
miguel alejandro sislian suez <Kentucky@MacBook-Pro-de-miguel-3.local>
2020-04-18 21:03:40 +0200

    7ºBotonoes ventana clientes, funciones y conexión implementadas

--- src/main/java/formularios/DialogCliente.java
+++ src/main/java/formularios/DialogCliente.java
@@ -1,6 +1,7 @@
 package formularios;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
@@ -13,6 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -54,7 +56,8 @@ public class DialogCliente extends JFrame {
 
 		menuSup();
 		crearCampos();
-		
+		crearBotonesDialog();
+
 		try {
 			DriverManager.registerDriver(new Driver());
 		} catch (SQLException ex) {
@@ -63,17 +66,165 @@ public class DialogCliente extends JFrame {
 
 	}
 
+	private void crearBotonesDialog() {
+		JPanel pnlBotons = new JPanel(new GridLayout(1, 3, 0, 0));
+		pnlBotons.setBorder(new EmptyBorder(0, 10, 10, 10));
+
+		JButton btnCrear = new JButton("Crear cliente");
+		btnCrear.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				actualizarCrearCliente(true);
+			}
+		});
+		pnlBotons.add(btnCrear);
+
+		JButton btnActualizar = new JButton("Actualizar cliente");
+		btnActualizar.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				actualizarCrearCliente(false);
+			}
+		});
+
+		pnlBotons.add(btnActualizar);
+
+		JButton btnEliminar = new JButton("Eliminar cliente");
+		btnEliminar.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+
+				eliminar();
+				
+			}
+		});
+		pnlBotons.add(btnEliminar);
+
+		add(pnlBotons, BorderLayout.SOUTH);
+
+	}
+
+	protected void eliminar() {
+		
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Acme?serverTimezone=Europe/Madrid", this.usuario,
+					this.contrasena);
+			
+			PreparedStatement ps = conn.prepareStatement("DELETE FROM Clientes WHERE DNI = ?");
+			ps.setString(1, txtDni.getText());
+			ps.executeUpdate();
+			JOptionPane.showMessageDialog(this, "Cliente borrado correctametne", "Todo OK", JOptionPane.INFORMATION_MESSAGE);
+			//llamamos al metodo limpiar datos
+			limpiarDatos();
+			
+		} catch (SQLException ex) {// SQLexcetion se da cunado no hay conewxion o algun error con la bbdd
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Error al cargar datos", JOptionPane.ERROR_MESSAGE);
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException ex1) {
+					System.out.println("Excepción. Cerrando conexión");
+				}
+			}
+		}
+	}
+
+
+	protected void actualizarCrearCliente(boolean isCrear) {// le decimos al boton actualizar que crear es falso
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Acme?serverTimezone=Europe/Madrid", this.usuario,
+					this.contrasena);
+
+			String consulta;
+
+			if (isCrear) {
+				consulta = "INSERT INTO Clientes (Nombre, Ape1, Ape2, Fec_Nac, DNI) VALUES (?,?,?,?,?)"; // consulta para insert
+			} else {
+				consulta = "UPDATE Clientes SET Nombre = ?, Ape1 = ?, Ape2 = ?, Fec_Nac = ? WHERE DNI = ? "; // consulta Update
+																											
+			}
+
+			PreparedStatement ps = conn.prepareStatement(consulta);
+			// pasamos los valores que queremos meter y hacemos omproaciones 
+			if (txtNombre.getText().equals("")) {
+				JOptionPane.showMessageDialog(this, "Introduzca un nombre", "Nombre de cliente",
+						JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			ps.setString(1, txtNombre.getText());
+
+			if (txtApellido1.getText().equals("")) {
+				JOptionPane.showMessageDialog(this, "Introduzca un primer apellido", "Apellido de cliente",
+						JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			ps.setString(2, txtApellido1.getText());
+
+			// si el campo del apl2 contiene algo escrito pasa el valor gettextapl2 y sino
+			// no devuelve un null
+			ps.setString(3, !txtApellido2.getText().equals("") ? txtApellido2.getText() : null);
+
+			if (txtFechaNacimiento.getText().equals("")) {
+				JOptionPane.showMessageDialog(this, "Introduzca fecha de nacimiento", "Fecha de nacimiento de cliente",
+						JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			ps.setString(4, txtFechaNacimiento.getText());
+
+			if (isCrear && txtDni.equals("")) {//sino el campo de dni esta vacio entonces estamos creando cliente
+				JOptionPane.showMessageDialog(this, "Introduzca DNI", "DNI de cliente", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			ps.setString(5, txtDni.getText());
+			
+			ps.executeUpdate();
+			String mensajeResult;
+			
+			if (isCrear) {
+				
+				mensajeResult = "Se ha creado el cliente correctamente";
+				txtDni.setEditable(false); //despues de crear el cliente ya no dejará modificar el dni
+			
+			}else {
+				
+				mensajeResult = "Se ha actualizado al cliente correctamente ";
+			}
+			
+			JOptionPane.showMessageDialog(this,  mensajeResult, "Todo OK", JOptionPane.INFORMATION_MESSAGE);
+			
+		} catch (SQLException ex) {// SQLexcetion se da cunado no hay conewxion o algun error con la bbdd
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Error al cargar datos", JOptionPane.ERROR_MESSAGE);
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException ex1) {
+					System.out.println("Excepción. Cerrando conexión");
+				}
+			}
+		}
+	}
+
 	private void crearCampos() {
 
 		pnlCampos = new JPanel();
-		pnlCampos.setLayout(new GridLayout(5,2,0,0));
-		pnlCampos.setBorder(new EmptyBorder(30,30,0,30));
-		
+		pnlCampos.setLayout(new GridLayout(5, 2, 0, 0));
+		pnlCampos.setBorder(new EmptyBorder(30, 30, 0, 30));
+
 		pnlCampos.add(new JLabel("DNI"));
 		txtDni = new JTextField();
 		pnlCampos.add(txtDni);
 
-		
 		pnlCampos.add(new JLabel("Nombre"));
 		txtNombre = new JTextField();
 		pnlCampos.add(txtNombre);
@@ -81,17 +232,17 @@ public class DialogCliente extends JFrame {
 		pnlCampos.add(new JLabel("Apellido1"));
 		txtApellido1 = new JTextField();
 		pnlCampos.add(txtApellido1);
-		
+
 		pnlCampos.add(new JLabel("Apellido2"));
 		txtApellido2 = new JTextField();
 		pnlCampos.add(txtApellido2);
-		
+
 		pnlCampos.add(new JLabel("Fecha de nacimiento"));
 		txtFechaNacimiento = new JTextField();
 		pnlCampos.add(txtFechaNacimiento);
-		
-		add(pnlCampos, BorderLayout.NORTH); //para que todos los campos que meta entren por arriba
-		
+
+		add(pnlCampos, BorderLayout.NORTH); // para que todos los campos que meta entren por arriba
+
 	}
 
 	private void menuSup() {
@@ -112,7 +263,6 @@ public class DialogCliente extends JFrame {
 			}
 		});
 
-		
 		// Cargar datos
 		JMenuItem cDatos = new JMenuItem("Cargar Datos");
 		cDatos.setMnemonic(KeyEvent.VK_D);
@@ -127,7 +277,6 @@ public class DialogCliente extends JFrame {
 			}
 		});
 
-		
 		// Limpiar datos
 		JMenuItem lDatos = new JMenuItem("Limpiar Datos");
 		lDatos.setMnemonic(KeyEvent.VK_L);
@@ -136,6 +285,8 @@ public class DialogCliente extends JFrame {
 		lDatos.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
+
+				limpiarDatos();
 
 			}
 		});
@@ -165,6 +316,27 @@ public class DialogCliente extends JFrame {
 
 	}
 
+	protected void limpiarDatos() {// poner el acceso n privado
+
+		Component[] componentes = pnlCampos.getComponents();
+
+		for (Component componente : componentes) {// del aarray componentes coge cada elemento
+			if (componente instanceof JTextField) {// comprobamos que el compoennte que recorremos es un textfield
+				JTextField jtf = (JTextField) componente;// parseamos / convertimos obj component a componente jtf
+				jtf.setText(""); // vaciamos el campo
+
+				// otro modo de hacer seria poniendo cada campo txtDni.serText("") pero sería
+				// más largo pero nos quitamos el bucle
+			}
+		}
+
+		// otro modo de hacer el bucle
+//		for (int i = 0; i < componentes.length; i++) {
+//			Component componente = componentes[i];
+//		}
+
+	}
+
 	protected void cargarDatos() {
 		Connection conn = null;
 
@@ -174,29 +346,36 @@ public class DialogCliente extends JFrame {
 
 			String dni = JOptionPane.showInputDialog(this, "Introduce el dni de cliente", "Busqueda de cliente",
 					JOptionPane.QUESTION_MESSAGE);
-			if(dni == null && dni.equals("")) {//si dni es diferente a null y a campo vacio sale del dialogo
+			if (dni == null || dni.equals("")) {// si dni es diferente a null y a campo vacio sale del dialogo
 				JOptionPane.showMessageDialog(this, "Introduce el dni de cliente", "Introduzca un dni",
 						JOptionPane.QUESTION_MESSAGE);
 				return;
 			}
-			
-			
-			//--------Buscamos clientes y datos a campos
-			PreparedStatement ps = conn.prepareStatement("SELECT Nombre, Ape1, Ape2, Fec_Nac FROM Clientes WHERE DNI = ?");
-			ps.setString(1, dni);//valor un que se gurda en resultst
+
+			// --------Buscamos clientes y datos a campos
+			PreparedStatement ps = conn
+					.prepareStatement("SELECT Nombre, Ape1, Ape2, Fec_Nac FROM Clientes WHERE DNI = ?");
+			ps.setString(1, dni);// valor un que se gurda en resultst
 			ResultSet rs = ps.executeQuery(); //
-			if (rs.first()) { //first mostrará la primera linea si exise, y coloca el cursor en esa línea (parece al next)
+			if (rs.first()) { // first mostrará la primera linea si exise, y coloca el cursor en esa línea
+								// (parece al next)
 				txtDni.setText(dni);
-				txtDni.setEditable(false);//aseguramos que no se pueda modificar el dni
+				txtDni.setEditable(false);// aseguramos que no se pueda modificar el dni
 				txtNombre.setText(rs.getString("Nombre"));
 				txtApellido1.setText(rs.getString("Ape1"));
-				txtApellido2.setText(rs.getObject("Ape2") == null ? "" : rs.getNString("Apellido2")); //hacemos la comprobacion con getObject, si es nula dejamos campo vacilo
-				//sino escribe apellido dos
+				txtApellido2.setText(rs.getObject("Ape2") == null ? "" : rs.getString("Ape2")); // hacemos la
+																										// comprobacion
+																										// con
+																										// getObject, si
+																										// es nula
+																										// dejamos campo
+																										// vacilo
+				// sino escribe apellido dos
 				txtFechaNacimiento.setText(rs.getDate("Fec_Nac").toString());
-				
-			}else {
-				JOptionPane.showMessageDialog(this, "No existe ningún cliente con ese DNI",
-						"Error en la búsqueda", JOptionPane.ERROR_MESSAGE);
+
+			} else {
+				JOptionPane.showMessageDialog(this, "No existe ningún cliente con ese DNI", "Error en la búsqueda",
+						JOptionPane.ERROR_MESSAGE);
 			}
 
 		} catch (SQLException ex) {// SQLexcetion se da cunado no hay conewxion o algun error con la bbdd
